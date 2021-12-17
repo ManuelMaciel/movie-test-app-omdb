029682ea687c8235d75247a6f37f1cfe47ca8eda
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-12-17 10:30:11 -0300

    feat; add context for fav movies and movies list

---
+++ src/context/MoviesContext.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useState } from 'react';
+import { searchAPI } from '../ts/OMDBAPI';
+import { buildHandleChanger } from '../ts/util';
+import { Movie, onHandleMoviesContextType } from '../types/types';
+
+const defaultValue={
+  ListMovies:[] as [Movie] | [],
+  infoMovie:{} as Movie,
+  imdbID:'' as string,
+  textSearch:'' as string,
+}
+
+export const MoviesContext= createContext(defaultValue);
+
+export const MoviesContextProvider=({children})=>{
+  const [state,setState] = useState(defaultValue);
+  const {textSearch} = state;
+  const onHandleChange=buildHandleChanger(defaultValue,setState) as onHandleMoviesContextType;
+
+  const verifySearch=(searchString:string, message:boolean)=>{
+    const canSearch= searchString.length>2;
+    if(message && !canSearch) alert('Escriba 3 letras como mínimo');
+    return canSearch;
+  }
+
+  const searchMovies=async (message=false,forced=true)=>{
+    if(verifySearch(textSearch,message)){
+      searchAPI('', { s: textSearch}).then((search) => {
+        console.log('search: ',search)
+        if (search.result && search.result?.Search) {
+          onHandleChange.ListMovies(search.result.Search);
+        }
+        else if(search.result?.Error) alert(`${search.result.Error}`)
+      });
+    }
+  }
+
+  const getInfoMovie=async (imdbID:string)=>{
+    const search=await searchAPI('', { i: imdbID });
+    if (search?.result) onHandleChange.infoMovie(search.result);
+    else if(search?.error) alert(`${search.error}`)
+  }
+
+  return(
+    <MoviesContext.Provider value={{...state,searchMovies,getInfoMovie,onHandleChange}}>
+      {children}
+    </MoviesContext.Provider>
+  )
+}
