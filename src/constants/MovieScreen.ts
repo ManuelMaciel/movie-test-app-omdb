e8ec57d0fb07b6684d44cf71eb24550bfd55de76
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-12-17 10:28:40 -0300

    feat: add constants

---
+++ src/constants/MovieScreen.ts
@@ -0,0 +1,14 @@
+export const MOVIE_INFOS = {
+    Type: 'Tipo',
+    Year: 'Año',
+    Released: 'Estreno',
+    imdbRating: 'IMDB Rating',
+    Genre: 'Género',
+    Language: 'Idioma',
+    Country: 'País',
+    Runtime: 'Runtime',
+    Rated: 'Puntuacion',
+    Director: 'Director',
+    Writer: 'Escritor',
+    Production: 'Producción',
+}
